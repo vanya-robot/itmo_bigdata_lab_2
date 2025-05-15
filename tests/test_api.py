9a6c173464eb7191311f18baa6c2eb6bfa9a7ec9
vanya-robot <alexeenkoyaroslav@mail.ru>
2025-05-15 13:11:58 +0300

    upd api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,27 @@
+import pytest
 from fastapi.testclient import TestClient
 from api.app import app
-import json
 
 client = TestClient(app)
 
-def test_predict_endpoint():
-    test_data = {
+@pytest.mark.parametrize("test_input,expected", [
+    ({
         "island": "Torgersen",
         "culmen_length_mm": 39.1,
         "culmen_depth_mm": 18.7,
         "flipper_length_mm": 181.0,
         "body_mass_g": 3750.0,
         "sex": "MALE"
-    }
-    response = client.post("/predict", json=test_data)
-    assert response.status_code == 200
-    assert "species" in response.json()
+    }, 200),
+    ({
+        "island": "Unknown",
+        "culmen_length_mm": -1,  # Invalid value
+        "culmen_depth_mm": 18.7,
+        "flipper_length_mm": 181.0,
+        "body_mass_g": 3750.0,
+        "sex": "MALE"
+    }, 422)
+])
+def test_predict_endpoint(test_input, expected):
+    response = client.post("/predict", json=test_input)
+    assert response.status_code == expected
